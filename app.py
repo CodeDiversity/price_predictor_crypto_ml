d54bb54b9315a916ae85959d7dbc1bb7b4933134
Michael Trew <40902786+mtrew2015@users.noreply.github.com>
2022-10-28 16:22:04 -0400

    changing to longer search periods

--- app.py
+++ app.py
@@ -17,15 +17,15 @@ if __name__ == '__app__':
 
 def predictCryptoPrice(coinId):
     coinId = coinId
-    response = requests.get(f"https://api.coingecko.com/api/v3/coins/{coinId}/market_chart?vs_currency=usd&days=1095&interval=daily")
+    response = requests.get(f"https://api.coingecko.com/api/v3/coins/{coinId}/market_chart?vs_currency=usd&days=1600&interval=daily")
     data = response.json()
     df = pd.DataFrame(data['prices'], columns=['ds', 'y'])
     df['ds'] = pd.to_datetime(df['ds'], unit='ms')
     model = Prophet()
     model.fit(df)
-    future = model.make_future_dataframe(periods=7)
+    future = model.make_future_dataframe(periods=30)
     forecast = model.predict(future)
-    print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(7))
-    return forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(7).to_dict(orient='records')
+    print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(30))
+    return forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(30).to_dict(orient='records')
     
 
